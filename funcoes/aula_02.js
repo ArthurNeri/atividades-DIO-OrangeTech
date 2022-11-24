68a3d36650b900096c11fc5cefdbc59c16829d44
ArthurNeri <arthurneri2004@gmail.com>
2022-11-24 01:47:36 -0300

    listaExercicio_03 aprimorada e aula_02 feita

---
+++ funcoes/aula_02.js
@@ -0,0 +1,22 @@
+/*
+// faça uma função que escreva o seu nome
+function escreverNome(nome) {
+    console.log('Meu nome é', nome);
+}
+
+// faça uma função que confere sua maioridade
+function conferirMaioridade(idade){
+    if (idade >= 18){
+        console.log('Tenho', idade, 'anos.','Sou maior de idade');
+    } else {
+        console.log('Tenho', idade, 'anos.','Sou menor de idade');
+    }
+}
+
+function main() {
+    escreverNome('Arthur');
+    conferirMaioridade(18);
+}
+main();
+*/
+// atividade da listaExercicio_03.js, adicionar funções:
